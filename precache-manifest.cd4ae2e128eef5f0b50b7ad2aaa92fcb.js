7454a247e2558fc2f5d0e4db29d4099b1bc1df89
Aditya Sharma <adityasharma56697@gmail.com>
2024-08-03 21:29:36 +0530

    Updates

---
+++ precache-manifest.cd4ae2e128eef5f0b50b7ad2aaa92fcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b9a00e81509bb85d3c96e9b2c14de3b",
+    "url": "/Amazon-clone/index.html"
+  },
+  {
+    "revision": "f59b37b35dde7aeac198",
+    "url": "/Amazon-clone/static/css/main.0ba18fa8.chunk.css"
+  },
+  {
+    "revision": "3fecd18839fe0f2ec255",
+    "url": "/Amazon-clone/static/js/2.1c47ec3c.chunk.js"
+  },
+  {
+    "revision": "29fe86816a802490cec5",
+    "url": "/Amazon-clone/static/js/3.d3e4d6ea.chunk.js"
+  },
+  {
+    "revision": "f59b37b35dde7aeac198",
+    "url": "/Amazon-clone/static/js/main.add7b81b.chunk.js"
+  },
+  {
+    "revision": "cb6eb1fb5b52f2a95ab9",
+    "url": "/Amazon-clone/static/js/runtime~main.e5384739.js"
+  }
+]);
